5c7816d66c6b7febe52b87e3ba16f6bffd2ebab4
Viktor <vik9269@mail.ru>
2023-03-10 21:45:36 +0700

    10

---
+++ homework_10/data.js
@@ -0,0 +1,24 @@
+const dataInfo = `
+[
+    {
+        "code": "USD",
+        "symbol": "$",
+        "description": "United States Dollar",
+        "url": "https://www.profinance.ru/currency_usd.asp",
+        "img": "https://www.ridus.ru/images/2022/9/26/1517532/in_article_webp_104a3c51f5.webp"
+    },
+    {
+        "code": "GBP",
+        "symbol": "&pound",
+        "description": "British Pound Sterling",
+        "url": "https://www.banki.ru/products/currency/gbp/",
+        "img": "https://myfin.by/source/1/jwqtJq7nFYL-Pu4uTa6qS6lry18NymLT.jpg"
+    },
+    {
+        "code": "EUR",
+        "symbol": "&euro",
+        "description": "Euro",
+        "url": "https://www.banki.ru/products/currency/eur/",
+        "img": "https://myfin.by/source/1/I2a9NgCYz6HpIVsu8nA-vOm0YiX9CirU.jpg"
+    }
+]`
